e56828e6268ff2acda50b40a0a198188686dfea4
lfs736012 <lfs736012@163.com>
2017-06-19 00:00:55 +0800

    模型化创建表单

--- codetime/views.py
+++ codetime/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import NameForm
 from . import models
 # Create your views here.
+# def get_name(request):
+# 	if request.method == 'POST':
+# 		form = NameForm(request.POST)
+# 		if form.is_valid():
+# 			return HttpResponse(content='1',mimetype='text/html',status=200,content_type='text/plain')
+# 	else:
+# 		form = NameForm()
+# 	return render(request, 'codetime/login.html',{'form': form})
 def login(request):
 	return render(request, 'codetime/login.html')
 
@@ -14,6 +22,9 @@ def login_action(request):
 	if name == person.name and password == person.password:
 		return render(request, 'codetime/product.html')
 	return render(request, 'codetime/login.html')
+def postProduct(request):
+	kehu = request.POST.get('kehu', 'KEHU')
+	
 
 def product(request):
 	return render(request, 'codetime/product.html')
