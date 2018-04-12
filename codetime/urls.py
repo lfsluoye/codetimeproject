4ef7a8f69cccd61dc1ecad40ac5af9264c087508
lfs <[lfs736012@163.com]>
2018-04-12 12:07:07 +0800

    修改设置

--- codetime/urls.py
+++ codetime/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-app_name = "codetime"
+app_name = 'codetime'
 urlpatterns = [
     url(r'^login/$', views.loginForm, name='loginForm'),
     url(r'^product/$', views.productForm, name='productForm'),
