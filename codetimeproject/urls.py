2987ad62a765472afdca492f0917afa7c459ce25
lifushuai <lfs736012@163.com>
2020-02-16 17:28:04 +0800

    首页改为静态界面

--- codetimeproject/urls.py
+++ codetimeproject/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from codetime import views
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^codetime/', include('codetime.urls', namespace="codetime")),
 ]
